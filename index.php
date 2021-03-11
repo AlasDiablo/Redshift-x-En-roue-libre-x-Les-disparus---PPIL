8572fc3660f0423cfd3d3c10362cca3f2ce815ed
AlasDiablo <25723276+alasdiablo@users.noreply.github.com>
2021-03-11 10:36:29 +0100

    Create rooting system, and View Rendering system
    Signed-off-by: AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+use ppil\util\AppContainer;
+use ppil\view\ViewRendering;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require __DIR__ . '/vendor/autoload.php';
+
+// Creation de l'application slim
+$app = AppContainer::getInstance();
+$app->addRoutingMiddleware();
+
+// Affichage des erreur (Dev Only)
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+// Creation de l'index (page pricipale)
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write(ViewRendering::render("Hello, world!", "Home page"));
+    return $response;
+})->setName('root');
