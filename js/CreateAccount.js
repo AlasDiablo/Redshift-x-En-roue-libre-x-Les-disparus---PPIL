44e39d61e6f53bc4f0a934ef2839f11464fff187
Mireille Vignal <32102096+MireilleVignal@users.noreply.github.com>
2021-03-18 10:41:43 +0100

    ajout du script js dans le html pour vérifier le formulaire

--- js/CreateAccount.js
+++ js/CreateAccount.js
@@ -1,7 +1,11 @@
 function verifMail() {
     let mail = document.getElementById("email").innerHTML;
 
-    if( !/^.+@.+[.].+$/.test(mail) ) {}
+    if( !/^.+@.+[.].+$/.test(mail) ) {
+        return false;
+    }
+
+    return true;
 }
 
 function verifMdp() {
@@ -9,18 +13,33 @@ function verifMdp() {
     let confirm = document.getElementById("confirmMdp").innerHTML;
 
     if(mdp != confirm) {
-        document.getElementById("mdp").
+        return false;
     }
+
+    return true;
 }
 
 function verifTel() {
     let tel = document.getElementById("tel").innerHTML;
 
-    if( tel.length != 10) {}
+    if(tel.length != 10) {
+        return false;
+    }
+
+    return true;
 }
 
 function verifForm() {
-    verifMail();
-    verifMdp();
-    verifTel();
+    mail = verifMail();
+    mdp = verifMdp();
+    tel = verifTel();
+
+    if(mail && mdp && tel) {
+        document.getElementById("form").submit();
+    }
+    else {
+        var newP = document.createElement("p");
+        var newContent = document.createTextNode("Veuillez saisir des valeurs correctes");
+        newP.appendChild(newContent);
+    }
 }
