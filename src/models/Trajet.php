4061a2ed336275991ba6190a73c24456912f3aac
AlasDiablo <25723276+alasdiablo@users.noreply.github.com>
2021-03-18 10:03:44 +0100

    Start created Trip and User models
    Signed-off-by: AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>

---
+++ src/models/Trajet.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace ppil\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Trajet extends Model
+{
+    protected $table = 'trajet';
+    protected $primaryKey = 'id_trajet';
+    public $incrementing = true;
+    public $timestamps = false;
+
+    public function conducteur()
+    {
+        return $this->hasOne(User::class, "email", "email_conducteur");
+    }
+}
