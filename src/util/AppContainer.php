d5587d3555632ffe75832348e91a6181e5615ec1
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2021-03-18 08:47:04 +0100

    Add minimalist documentation
    Signed-off-by: AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>

--- src/util/AppContainer.php
+++ src/util/AppContainer.php
@@ -2,12 +2,16 @@
 
 namespace ppil\util;
 
+use Slim\App;
 use Slim\Factory\AppFactory;
 
 class AppContainer
 {
     private static $app = null;
 
+    /**
+     * @return App|null Instance de l'application slim
+     */
     public static function getInstance()
     {
         if (null === self::$app) {
