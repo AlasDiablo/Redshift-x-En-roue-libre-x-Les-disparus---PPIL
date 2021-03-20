882a3076c04793f23516fa368c0135cbc3de34ea
AlasDiablo <25723276+alasdiablo@users.noreply.github.com>
2021-03-20 18:35:52 +0100

    Create Relationship on User
    Signed-off-by: AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>

--- src/models/Utilisateur.php
+++ src/models/Utilisateur.php
@@ -15,6 +15,24 @@ class Utilisateur extends Model
 
     public function mesTrajets()
     {
-        return $this->hasMany(Trajet::class, "email_conducteur", "email");
+        return $this->hasMany(Trajet::class, 'email_conducteur', 'email');
+    }
+
+    public function mesParticipation()
+    {
+        return $this->belongsToMany(Trajet::class, 'passager','email_passager', 'id_trajet', 'email', 'id_trajet');
+    }
+
+    public function memberDe()
+    {
+        return $this->belongsToMany(Groupe::class, 'membre', 'id_groupe', 'email_membre', 'email', 'id_groupe');
+    }
+
+    public function notificationRecu() {
+        return $this->hasMany(Notification::class, 'utilisateur', 'email');
+    }
+
+    public function notificationEnvoyer() {
+        return $this->hasMany(Notification::class, 'emeteur', 'email');
     }
 }
