c803738804c362db70150dc3c69c5a05ddc0abc9
lindsayNyr <43404297+lindsayNyr@users.noreply.github.com>
2021-03-21 23:54:18 +0100

    Update UserController.php
    seDeconnecter -> a voir (session ?) 
    mdpOublie -> a tester 
    récupérerMdp -> a finir et a tester
    envoieEmail -> a tester

--- src/controller/UserController.php
+++ src/controller/UserController.php
@@ -30,9 +30,85 @@ class UserController
 
     public static function seDeconnecter()
     {
+        session_unset(); 
+        session_destroy(); 
+        //ajouter l'url de la page d'acceuille 
+        header("Location : index ?")
+        exit(); 
+
     }
 
     public static function mdpOublie()
     {
+
+        
+        $mail = filter_var($_POST["mail"], FILTER_DEFAULT);
+        
+        $value = Utilisateur::where("email", $mail)->get()->first();
+        if (!isset($value)) {
+            return UserView::erreurPost();
+        }
+
+
+        $body = "cliquez sur l'url ci dessous pour réinitialisez votre mdp : ...";
+        $nom = Utilisateur::select("nom")->where("email", "=", $mail)->first(); 
+
+
+        envoieEmail($body, "reinitialisez le mdp", $mail, $nom  )
+    }
+
+
+    public static function recupererMdp(){
+
+
+        $mdp1 = filter_var($_POST["mdp?"], FILTER_DEFAULT);
+        $mdp2 = filter_var($_POST["mdp?"], FILTER_DEFAULT);
+
+        if(isset($mdp1) && isset($mdp2)){
+
+
+            if($mdp1 == $mdp2 /*&& verification des contraintes mdp*/){
+
+
+                //Aucune idée de comment recuperer l'email de la personne 
+
+                //changer le mdp dans la base de donnée, pas sur ? 
+                Utilisateur::where("email", "=", $mail)->update(["mdp" => $mdp1]); 
+
+
+                //rediriger vers la page de connexion 
+                header("Location : ?"); 
+
+            }
+        }
+    }
+
+
+    //peut etre mettre la fonction dans un fichier a part ? 
+    private static function envoieEmail($body, $title, $email, $name){
+    
+        $mail = new PHPMailer();
+
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;
+        $mail->IsSMTP();
+        $mail->Mailer     = "smtp";
+        $mail->SMTPDebug  = 1;
+        $mail->SMTPAuth   = TRUE;
+        $mail->SMTPSecure = "tls";
+        $mail->Port       = 587; 
+        $mail->Host       = "smtp.gmail.com";
+        $mail->Username   = "chiken.killer.lel@gmail.com"; 
+        $mail->Password   = file_get_contents(Reference::EMAIL_INI); // C'est le fichier avec le mdp du compte gmail
+        $mail->setFrom("no-repley@sharemyride.fr", "ShareMyRide"); // Qui l'envoie
+
+        $mail->addAddress($email, $name); 
+        $mail->isHTML(false); 
+        $mail->setLanguage("fr");
+        $mail->CharSet    = "UTF-8"; 
+        $mail->Subject    = $title; 
+        $mail->Body       = $body;
+        if(!$mail->send()){
+            echo "erreur envoie mail";
+        }
     }
 }
