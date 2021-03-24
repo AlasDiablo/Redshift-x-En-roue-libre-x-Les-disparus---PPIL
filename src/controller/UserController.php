2d8bda650275b036461c0793bc9efbf26d288a7d
Arthur Debart <debart4u@etu.univ-lorraine.fr>
2021-03-24 17:09:30 +0100

    Ajout du traitement du formulaire et modification des récupérations des valeurs POST.

--- src/controller/UserController.php
+++ src/controller/UserController.php
@@ -8,14 +8,40 @@ use ppil\util\AppContainer;
 class UserController  extends Controller
 {
 	public function creerUtilisateur(){
-		$nom = htmlentities($_POST['name']);
-		$prenom = htmlentities($_POST['firstName']);
-		$email = htmlentities($_POST['mail']);
-		$mdp = htmlentities($_POST['password']);
-		$tel = htmlentities($_POST['phone']);
-		$sexe = htmlentities($_POST['sex']);
-		$a_voiture = htmlentities($_POST['car']);
-		if(!filter_var($email, FILTER_VALIDATE_EMAIL) || !filter_var($nom) || !filter_var($prenom) || !filter_var($mdp) || !filter_var($tel) || !filter_var($sexe) || !filter_var($a_voiture)){
+		$nom = filter_var($_POST['name'], FILTER_DEFAULT);
+		$prenom = filter_var($_POST['firstName'], FILTER_DEFAULT);
+		$email = filter_var($_POST['mail'], FILTER_DEFAULT);
+		$mdp = filter_var($_POST['password'], FILTER_DEFAULT);
+		$mdpconf = filter_var($_POST['confirmpassword'], FILTER_DEFAULT);
+		$tel = filter_var($_POST['phone'], FILTER_DEFAULT);
+		$sexe = filter_var($_POST['sex'], FILTER_DEFAULT);
+		$a_voiture = filter_var($_POST['car'], FILTER_DEFAULT);
+
+		if(!isset($nom) || !preg_match("#^[a-zA-Z]+$#", $nom) || strlen($nom < 2) || strlen($nom > 25)){
+			return UserView::erreurPost();
+		}
+
+		if(!isset($prenom) || !preg_match("#^[a-zA-Z]+$#", $prenom) || strlen($prenom < 2) || strlen($prenom > 25)){
+			return UserView::erreurPost();
+		}
+
+		if(!isset($email) || !filter_var($email, FILTER_VALIDATE_EMAIL){
+			return UserView::erreurPost();
+		}
+
+		if(!isset($mdp) || !isset($mdpconf) || ($mdp != $mdpconf) || strlen($mdp) < 7 || strlen($mdpconf) < 7){
+			return UserView::erreurPost();
+		}
+
+		if(!isset($tel) || strlen($tel) != 10 || !preg_match("#[0][6][- \.?]?([0-9][0-9][- \.?]?){4}$#", $tel)){
+			return UserView::erreurPost();
+		}
+
+		if(!isset($sexe)){
+			return UserView::erreurPost();
+		}
+
+		if(!isset($a_voiture)){
 			return UserView::erreurPost();
 		}
 		
@@ -30,13 +56,13 @@ class UserController  extends Controller
 			'sexe' => $sexe,
 			'a_voiture' => $a_voiture,
 			'note' => 5,
-			'activer_notification' => 'N',
+			'activer_notif' => 'N',
 		]);
 		
 		$_SESSION['mail'] = $mail;
+
         $url = AppContainer::getInstance()->getRouteCollector()->getRouteParser()->urlFor('root');
         header("Location: $url");
-        exit();*/
+        exit();
 	}
-
 }
