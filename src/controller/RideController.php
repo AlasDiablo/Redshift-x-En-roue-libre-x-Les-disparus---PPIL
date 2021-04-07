f6e90f7cb9c8f1d339bce053e06868da0eaf687f
AlasDiablo <25723276+alasdiablo@users.noreply.github.com>
2021-04-07 16:07:09 +0200

    Reformat and fix missing php code on RideController
    Signed-off-by: AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>

--- src/controller/RideController.php
+++ src/controller/RideController.php
@@ -1,27 +1,33 @@
 <?php
-	public static function getRide($id)
-	{
-		$trajet = Trajet::where('id_trajet', '=', $id)->first();
-		return $trajet;
-	}
-	
-	public static function getEtape($id)
-	{
-		$etapes = Ville_intermediaire::where('id_trajet', '=', $id)->get();
-		return $etapes;
-	}
-	
-	
-	public static function getPassager($id)
-	{
-		$passagers = Passager::where('id_trajet', '=', $id)->get();
-		return $passagers;
-	}
-	
-	public static function getNbPlaceOccupee($id)
-	{
-		$nbPassager = count(getPassager($id));
-		return $nbPassager;
-	}
-	
-?>
+
+namespace ppil\controller;
+
+use ppil\models\Passager;
+use ppil\models\Trajet;
+use ppil\models\VilleIntermediaire;
+
+class RideController
+{
+
+    public static function getRide($id)
+    {
+        return Trajet::where('id_trajet', '=', $id)->first();
+    }
+
+    public static function getEtape($id)
+    {
+        return Villeintermediaire::where('id_trajet', '=', $id)->get();
+    }
+
+    public static function getPassager($id)
+    {
+        return Passager::where('id_trajet', '=', $id)->get();
+    }
+
+    public static function getNbPlaceOccupee($id)
+    {
+        return count(self::getPassager($id));
+    }
+
+
+}
